367600bdda55bf7eb4f04ce6dd628177f7a6e2c9
ASHRAF\ashraf <canini9720@gmail.com>
2020-04-12 17:22:03 +0400

    auth-service: New implementation

--- supra-micro-auth/src/main/java/com/supra/AuthServiceApplication.java
+++ supra-micro-auth/src/main/java/com/supra/AuthServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableAuthorizationServer
+//@EnableDiscoveryClient
+//@EnableResourceServer
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
