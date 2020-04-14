1a5bb958f9524f120e01ca9de06dac401eb346c9
ASHRAF\ashraf <canini9720@gmail.com>
2020-04-14 22:39:28 +0400

    supra-ui : For to test authorizaion code - flow

---
+++ supra-ui/src/main/java/com/supra/SupraUiApplication.java
@@ -0,0 +1,13 @@
+package com.supra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupraUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupraUiApplication.class, args);
+	}
+
+}
---
+++ supra-ui/src/test/java/com/supra/SupraUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupraUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
